0342244e55491c7a5215b44bc46e60114a8ecd6f
mfonsos <mfonsos@gmail.com>
2012-01-18 19:04:44 -0500

    added a pages controller

---
+++ rail_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailTest::Application.config.secret_token = '1ad93a070bdd85bf0296d22c98941eeebef2cc7c10d7e18847cd2ddc1f0c7519c0af2c7238b2c8b9fb84586c6dfc1a1010eb0b24ebe1d62c50f6e3146c9df640'
--- rail_test/config/routes.rb
+++ rail_test/config/routes.rb
@@ -1,8 +1,4 @@
-SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
+RailTest::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
